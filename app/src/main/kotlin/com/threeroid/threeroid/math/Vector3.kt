fa54deada91b24640757f9d0f5c5638064e23de7
mitsuo0114 <oono0114@gmail.com>
2018-12-14 09:33:51 +0900

    add simple calculation for Vector3

--- app/src/main/kotlin/com/threeroid/threeroid/math/Vector3.kt
+++ app/src/main/kotlin/com/threeroid/threeroid/math/Vector3.kt
@@ -6,6 +6,13 @@ package com.threeroid.threeroid.math
 data class Vector3(var x: Float = 0.0f, var y: Float = 0.0f, var z: Float = 0.0f) {
     constructor(x: Int, y: Int, z: Int) : this(x.toFloat(), y.toFloat(), z.toFloat())
 
+    companion object {
+        fun multiplyVectors(v1: Vector3, v2: Vector3): Vector3 {
+            return Vector3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z)
+        }
+    }
+
+
     fun set(x: Float, y: Float, z: Float): Vector3 {
         this.x = x
         this.y = y
@@ -19,4 +26,20 @@ data class Vector3(var x: Float = 0.0f, var y: Float = 0.0f, var z: Float = 0.0f
         this.z = scalar
         return this
     }
+
+    fun add(other: Vector3): Vector3 {
+        this.x += other.x
+        this.y += other.y
+        this.z += other.z
+        return this
+    }
+
+    fun addScaledVector(other: Vector3, scalar: Float): Vector3 {
+        this.x += other.x * scalar
+        this.y += other.y * scalar
+        this.z += other.z * scalar
+        return this
+    }
+
+
 }
--- app/src/test/kotlin/com/threeroid/threeroid/math/Vector3Test.kt
+++ app/src/test/kotlin/com/threeroid/threeroid/math/Vector3Test.kt
@@ -24,4 +24,40 @@ class Vector3Test {
         assertTrue(target1.equals(target2))
     }
 
+    @Test
+    fun add() {
+        val target1 = Vector3(0.0f, 0.0f, 0.0f)
+        val target2 = Vector3(1.0f, 2.0f, 3.0f)
+
+        target1.add(target2)
+
+        assertTrue(target1.x == 1.0f)
+        assertTrue(target1.y == 2.0f)
+        assertTrue(target1.z == 3.0f)
+    }
+
+    @Test
+    fun addScaledVector() {
+        val target1 = Vector3(0.0f, 0.0f, 0.0f)
+        val target2 = Vector3(1.0f, 2.0f, 3.0f)
+
+        target1.addScaledVector(target2, 4.0f)
+
+        assertTrue(target1.x == 4.0f)
+        assertTrue(target1.y == 8.0f)
+        assertTrue(target1.z == 12.0f)
+    }
+
+    @Test
+    fun multiplyVectors() {
+        val target1 = Vector3(4.0f, 4.0f, 4.0f)
+        val target2 = Vector3(1.0f, 2.0f, 3.0f)
+
+        val result = Vector3.multiplyVectors(target1, target2)
+
+        assertTrue(result.x == 4.0f)
+        assertTrue(result.y == 8.0f)
+        assertTrue(result.z == 12.0f)
+    }
+
 }
